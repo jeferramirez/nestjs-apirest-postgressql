ff2264638a1c0c320bb839eeffca2b5f36d6c8d5
Jefer Ramirez <jefer.ramirezjrt@hotmail.com>
2019-04-07 18:32:02 -0500

    api rest con postgress beta 1.0

--- src/tareas/tarea.controller.ts
+++ src/tareas/tarea.controller.ts
@@ -1,4 +1,87 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, Get, Param, Delete, Put } from '@nestjs/common';
+import { TareaService } from './tarea.service';
 
 @Controller('tarea')
-export class TareaController {}
+export class TareaController {
+
+
+    constructor( private tareaService: TareaService ) {}
+
+    @Post()
+     async create(@Body() tarea: any, @Res()resp ) {
+   
+       try {
+       const newtarea =  await this.tareaService.createUser(tarea);
+       resp.status(HttpStatus.CREATED).json(newtarea);
+   
+       } catch (error) {
+   
+        resp.status(HttpStatus.FORBIDDEN).json({mensaje: 'error en la creación de la tarea'});
+       }
+   
+   
+     }
+   
+     @Get()
+     async getAll( @Res()res ) {
+   
+       try {
+           const usuarios =  await this.tareaService.findAll();
+           res.status(HttpStatus.OK).json(usuarios);
+       } catch (error) {
+           res.status(HttpStatus.FORBIDDEN).json({mensaje: 'error obteniendo las tareas'});
+   
+       }
+     }
+   
+   
+     @Get(':id')
+     async getOne( @Res() res, @Param('id')idTarea ) {
+   
+       try {
+           const newtarea =  await this.tareaService.findOne(idTarea);
+           res.status(HttpStatus.OK).json(newtarea);
+       } catch (error) {
+           res.status(HttpStatus.FORBIDDEN).json({mensaje: 'error obteniendo usuarios'});
+   
+       }
+     }
+
+
+     @Get(':id/todas')
+     async getByUser( @Res() res, @Param('id')idUser ) {
+   
+       try {
+           const newtarea =  await this.tareaService.findByUser(idUser);
+           res.status(HttpStatus.OK).json(newtarea);
+       } catch (error) {
+           res.status(HttpStatus.FORBIDDEN).json({mensaje: 'error obteniendo usuarios'});
+   
+       }
+     }
+   
+     @Delete(':id')
+     async deletdOne( @Res() res, @Param('id')idTarea ) {
+   
+       try {
+           const tarea =  await this.tareaService.deleteOne(idTarea);
+           return res.status(HttpStatus.OK).json(tarea);
+       } catch (error) {
+           return  res.status(HttpStatus.FORBIDDEN).json({mensaje: 'error BORRANDO Tarea '});
+    
+       }
+     }
+   
+     @Put()
+     async updated(@Res() res, @Body()tarea ) {
+   
+       try {
+           const newtarea =  await this.tareaService.updatedUser(tarea);
+           return res.status(HttpStatus.OK).json(newtarea);
+       } catch (error) {
+           return  res.status(HttpStatus.FORBIDDEN).json({mensaje: 'error editando el usuario '});
+    
+       }
+     }
+
+}
--- src/tareas/tarea.service.ts
+++ src/tareas/tarea.service.ts
@@ -20,6 +20,15 @@ export class TareaService {
         return await this.tareaRepository.findOne( idTarea );
       }
     
+
+      async findByUser( usuario_id: number) {
+
+        try {
+          return await this.tareaRepository.find({ usuario_id });
+        } catch (err) {
+          return err;
+        }
+      }
     
       async createUser(tareaNueva: any): Promise<Tarea> {
        
@@ -27,9 +36,9 @@ export class TareaService {
     
        nuevo.descripcion = tareaNueva.descripcion;
        nuevo.fecha_creacion = tareaNueva.fecha_creacion;
+       nuevo.fecha_ejecucion = tareaNueva.fecha_ejecucion;
        nuevo.estado = tareaNueva.estado;
        nuevo.usuario_id = tareaNueva.usuario_id;
-       nuevo.fecha_ejecucion = tareaNueva.fecha_ejecucion;
     
     
        return await this.tareaRepository.save(nuevo);
@@ -38,12 +47,10 @@ export class TareaService {
       async updatedUser(tareaNueva: any) {
        
         // const nuevo = new Usuario();
-        const tareaActualizar = await this.tareaRepository.findOne(tareaNueva.id);
-    
+        const tareaActualizar = await this.tareaRepository.findOne(tareaNueva.id);  
         tareaActualizar.descripcion = tareaNueva.descripcion;
         tareaActualizar.fecha_ejecucion = tareaNueva.fecha_ejecucion;
         tareaActualizar.estado = tareaNueva.estado;
-    
         return await this.tareaRepository.save(tareaActualizar);
     
     
